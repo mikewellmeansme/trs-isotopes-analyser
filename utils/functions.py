73bf77ff01c0c01763dab983b183040f53f4164f
Mikhail <fff_khhh@mail.ru>
2021-11-30 16:11:33 +0700

    upd

--- utils/functions.py
+++ utils/functions.py
@@ -1,9 +1,18 @@
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 
-def dropna_pearsonr(x, y):
+def dropna(x,y):
     x, y = np.array(x), np.array(y)
     nas = np.logical_or(np.isnan(x), np.isnan(y))
     x, y = x[~nas], y[~nas]
+    return x, y
+
+def dropna_pearsonr(x, y):
+    x, y = dropna(x,y)
     r, p = pearsonr(x, y)
+    return r, p
+
+def dropna_spearmanr(x, y):
+    x, y = dropna(x,y)
+    r, p = spearmanr(x, y)
     return r, p
