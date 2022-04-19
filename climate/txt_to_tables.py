5c764af6a9d693c30eee1cff4f8f77eef556faa1
Mikhail <fff_khhh@mail.ru>
2022-04-19 22:18:48 +0700

    new climate data

--- climate/txt_to_tables.py
+++ climate/txt_to_tables.py
@@ -5,22 +5,30 @@ import pandas as pd
 # Топорные ибо требуют ручного ввода имён колонок
 # В будущем хорошо бы доработать, но они вроде как одноразовые поэтому пока сойдёт
 
-daily_columns_names = [
+daily_columns_names_ru = [
     "Индекс ВМО", "Год", "Месяц", "День",
     "Общий признак качества температур",
     "Минимальная температура воздуха",
     "Средняя температура воздуха",
     "Максимальная температура воздуха",
     "Количество осадков"
-    ]
+]
 
-montly_columns_names = [
+montly_columns_names_ru = [
     "Индекс ВМО", "Год",
     "Январь", "Февраль", "Март",
     "Апрель", "Май", "Июнь",
     "Июль", "Август", "Сентябрь",
     "Октябрь", "Ноябрь", "Декабрь"
-    ]
+]
+
+montly_columns_names_en = [
+    "WMO Index", "Year" ,
+    "January", "February", "March",
+    "April", "May", "June",
+    "July", "August", "September",
+    "October", "November", "December"
+]
 
 def delete_all_spaces_from_txt(file_path: str) -> None:
     with open(file_path, 'r+') as f:
@@ -51,7 +59,7 @@ def txt_to_df(station: str, chars: str,
 # https://climexp.knmi.nl/selectfield_obs2.cgi?id=someone@somewhere
 # в DataFrame
 def dat_to_df(file_path: str, monthly=True, save_csv=True, save_ecel=False):
-    df = pd.DataFrame(columns=montly_columns_names[1:] if monthly else ['Year', 'Month', 'Day', 'Measurement'])
+    df = pd.DataFrame(columns=montly_columns_names_en[1:] if monthly else ['Year', 'Month', 'Day', 'Measurement'])
     with open(file_path, 'r') as f:
         text = f.readlines()
         for line in text:
