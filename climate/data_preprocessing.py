4cea70c6331c17e4d0d8d6583b247d2cb3042ff2
Mikhail <fff_khhh@mail.ru>
2022-05-14 17:21:19 +0700

    add loaded data checking

--- climate/data_preprocessing.py
+++ climate/data_preprocessing.py
@@ -1,14 +1,24 @@
 import pandas as pd
 from os import listdir
 
+needed_columns = ['Year', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 characteristics = [
-        'precipitation',
-        'relative humidity',
-        'sunshine duration',
-        'temperature',
-        'vpd'
-    ]
+    'precipitation',
+    'relative humidity',
+    'sunshine duration',
+    'temperature',
+    'vpd'
+]
+
+class WrongMontlyDataColumns(Exception):
+    pass
+
+
+def check_monthly_climate_df(df):
+    global needed_columns
+    return set(needed_columns) == set(df.columns)
+
 
 def load_data(path='input/climate/real', characteristics=characteristics):
 
@@ -16,11 +26,17 @@ def load_data(path='input/climate/real', characteristics=characteristics):
 
     for char in characteristics:
         for file in listdir(f'{path}/{char}'):
-            loc_df = pd.read_csv(f'{path}/{char}/{file}')
+            file_path = f'{path}/{char}/{file}'
+            loc_df = pd.read_csv(file_path)
+
             if 'WMO Index' in loc_df.columns:
                 loc_df = loc_df.drop(columns=['WMO Index'])
             if 'Индекс ВМО' in loc_df.columns:
                 loc_df = loc_df.drop(columns=['Индекс ВМО'])
+            
+            if not check_monthly_climate_df(loc_df):
+                raise WrongMontlyDataColumns(f'File {file_path} does not match the monthly df climate format!')
+
             data[file.split('.')[0]] = loc_df
     
     return data
