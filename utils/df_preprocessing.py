6c2fdcd2de87821efb358b293b53595b6b271e0d
Mikhail <fff_khhh@mail.ru>
2022-05-14 22:57:16 +0700

    add real grid comparison functions

--- utils/df_preprocessing.py
+++ utils/df_preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 from datetime import date
 
 month_names = ['September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August']
@@ -89,6 +91,8 @@ def highlight_significant_cells(x):
     return None
 
 
+# Functions for Real \ Grid data comparison
+
 def get_climate_df_mean(df, columns=month_names):
     df = monthly_climate_offset_and_clean(df)
     df['Mean'] = df[columns].mean(axis=1, skipna=True)
@@ -99,3 +103,66 @@ def get_climate_df_sum(df, columns=month_names):
     df = monthly_climate_offset_and_clean(df)
     df['Sum'] = df[columns].sum(axis=1, skipna=True)
     return df[['Year', 'Sum']]
+
+
+def merge_dfs(left_df, right_df, on='Year', suffixes=('_real', '_grid')):
+    res = pd.merge(left_df, right_df, how='outer', on=on, suffixes=suffixes).sort_values(by=on).reset_index(drop=True)
+    return res
+
+
+def get_real_grid_comparison(df_real:pd.DataFrame, df_grid:pd.DataFrame, mean_method='mean'):
+    mean_real = get_climate_df_mean(df_real) if mean_method=='mean' else get_climate_df_sum(df_real)
+    mean_grid = get_climate_df_mean(df_grid) if mean_method=='mean' else get_climate_df_sum(df_grid)
+    temp = merge_dfs(mean_real, mean_grid).to_numpy().T
+    years = temp[0]
+    real = temp[1]
+    grid = temp[2]
+    return years, real, grid
+
+
+def plot_total_real_grid_comparison(data_real:dict, data_grid:dict, stations:list, char2label:dict, char2mean_method:dict, ylims:dict):
+    """
+    data_real, data_grid: load_data from input folders
+
+    stations = ['Chokurdakh', 'Khatanga', 'Inuvik']
+
+    char2label = {
+        'Temp':'Average temperature (°C)',
+        'Prec':'Total precipitation\n (mm)',
+        'VPD': 'Average\n vapour pressure deficit\n (mbar)'
+    }
+
+    ylims = {
+        'Temp':[-18, -4],
+        'Prec':[50, 420],
+        'VPD': [1,5]
+    }
+
+    char2mean_method = {
+        'Temp': 'mean',
+        'Prec': 'sum',
+        'VPD': 'mean'
+    }
+    """
+    stations_amount = len(stations)
+    chars_amount = len(char2label)
+
+    fig, axes = plt.subplots(nrows=chars_amount, ncols=stations_amount, sharex=True, sharey='row', dpi=300, figsize=(12, 8))
+    plt.subplots_adjust(hspace=0.06, wspace=0.06)
+
+    for i, char in enumerate(char2label):
+        for j, station in enumerate(stations):
+            key = f'{char}_{station}'
+            loc_real = data_real[key]
+            loc_grid = data_grid[key]
+            years, real, grid = get_real_grid_comparison(loc_real, loc_grid, char2mean_method[char])
+            axes[i,j].plot(years, grid, label='Grid')
+            axes[i,j].plot(years, real, label='Real')
+            axes[i,j].set_ylim(ylims[char])
+
+            axes[0, j].set_title(station)
+            axes[chars_amount-1, j].set_xlabel('Year')
+
+        axes[i, 0].set_ylabel(char2label[char])
+    
+    return fig, axes
