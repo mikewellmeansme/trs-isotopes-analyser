df2195bbd1a485f9bf8dc2ad907c787c1b4cacc3
Mikhail <fff_khhh@mail.ru>
2022-05-30 15:36:34 +0700

    remove deprecated

--- climate/coh.py
+++ climate/coh.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from climate.dendroclim import (
-    plot_mothly_dendroclim,
     get_multiple_monthly_dendroclim,
     plot_multiple_monthly_dendroclim
 )
@@ -21,84 +20,6 @@ def get_multiple_values(d: dict, keys: list) -> list:
             result.append(value)
             existing_keys.append(key)
     return existing_keys, result
-
-
-def get_coh_corr_table(climate_data:dict, df_COH: pd.DataFrame,
-                       locs: list, regs: list, ind_titels: dict, char_to_color: dict,
-                       fig_savepath:str='', years: str='', ylim0:float=-.7, ylim1:float=.8) -> pd.DataFrame:
-    """
-    Функция построения таблицы помесячной корреляции между данными по C, O, H и климатическими данными
-
-    climate_data: dict в котором ключи имеют вид Показатель_Станция, а значения -- DataFrame с климатическими данными
-    df_COH: DataFrame с колонками Year и C, O, H по регионам в формате Регион_Показатель
-    locs: Список названий метеостанкий (Station name in Sites.csv)
-    regs: Список кодовых названий участков (Site code in Sites.csv)
-    ind_titels: dict с показателями COH в ключах и LaTEX формат их отображения на графиках в значениях.
-    Пример:
-        ind_titels = {
-            '2H': '$δH$',
-            '13C': '$δ^{13}C$',
-            '18O': '$δ^{18}O$'
-        }
-    char_to_color: dict с климатическими характеристиками в ключах и цветами для их отображения на графиках в значениях
-    Пример:
-        char_to_clolr = {
-            'Temp': 'red',
-            'Prec': 'blue',
-            'VPD': 'green',
-            'SD': 'orange',
-            'RH': 'lightblue'
-        }
-    fig_savepath: Путь, по которому будут сохранены графики (если '', То не будут сохранены)
-    years: Годы, за которые строится измерение
-    ylim0: Нижняя граница графика по оси y
-    ylim1: Верхняя граница графика по оси y
-    """
-
-    r_values, p_values = dict(), dict()
-    for ind in ind_titels:
-        for loc, reg in zip(locs, regs):
-            column = f'{reg}_{ind}'
-            if not column in df_COH.columns:
-                continue
-            title = f'{loc} {ind_titels[ind]} {years}'
-            keys, chars = get_multiple_values(climate_data, [f'Temp_{loc}', f'Prec_{loc}', f'VPD_{loc}', f'SD_{loc}', f'RH_{loc}'])
-            keys = [key.split('_')[0] for key in keys]
-            colors = [char_to_color[key] for key in keys]
-
-            fig, ax, rs, ps = plot_mothly_dendroclim(
-                df_crn=df_COH[['Year', column]],
-                dfs_char=chars,
-                ylabels=keys,
-                colors=colors,
-                title=title,
-                ylim=[ylim0, ylim1],
-                savepath=fig_savepath
-            )
-            r_values[f'{column} {loc}'] = rs
-            p_values[f'{column} {loc}' ] = ps
-
-    df_COH_corr = {
-    'Month' : {'':  month_names},
-    }
-    for column in r_values:
-        df_COH_corr[column] = dict()
-        loc = column.split()[-1]
-        for char in ['Temp', 'Prec', 'VPD', 'SD', 'RH']:
-            if f'{char}_{loc}' in climate_data:
-                df_COH_corr[column][char] = []
-
-        rs = r_values[column]
-        ps = p_values[column]
-        for key in rs:
-            if key in df_COH_corr[column]:
-                for r,p in zip(rs[key], ps[key]):
-                    text = f'{r:.2f}\n(p={p:.3f})'
-                    df_COH_corr[column][key].append(text)
-
-    df_reform = {(outerKey, innerKey): values for outerKey, innerDict in df_COH_corr.items() for innerKey, values in innerDict.items()}
-    df_reform = pd.DataFrame(df_reform)
-    return df_reform
 
 
 def get_coh_corr(climate_data:dict, df_COH: pd.DataFrame,
