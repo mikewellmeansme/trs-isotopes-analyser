fd74434d976f56512856a06c2a1cb3d804772508
Mikhail <fff_khhh@mail.ru>
2022-05-30 19:08:09 +0700

    add plot_multiple_coh_corr and TODO

--- climate/coh.py
+++ climate/coh.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from climate.dendroclim import (
     get_multiple_monthly_dendroclim,
     plot_multiple_monthly_dendroclim
 )
+from utils.plots import (
+    combine_hex_colors,
+    month_names,
+    m_names,
+    ind_titels,
+    char_2_characteristic
+)
 
 pd.options.mode.chained_assignment = None 
 
-month_names = ['September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August']
+#TODO: Add comments and function signatures
 
 def get_multiple_values(d: dict, keys: list) -> list:
     """
@@ -57,6 +66,43 @@ def plot_coh_corr(r_values, p_values, ind_titels: dict, char_to_color: dict,
 
         plot_multiple_monthly_dendroclim(rs, ps, colors, title, [ylim0, ylim1], fig_savepath)
 
+
+#TODO: Refactor
+def plot_multiple_coh_corr(r_values, p_values, char, ind, ylim0:float=-.75, ylim1:float=1.0):
+    fig, ax = plt.subplots(figsize=(6,5), dpi=300)
+    plt.subplots_adjust(right=0.9)
+
+    max_i = 0
+    for key in r_values:
+        if ind in key:
+            if char in r_values[key]:
+                max_i+=1
+
+    colors = []
+
+    for i in range(1, max_i+1):
+        color = combine_hex_colors({"ff1100": 1.0 - i*(1.0/max_i), "0012ff": i*(1.0/max_i)})
+        colors.append(color)
+
+    i = 0
+    for key in r_values:
+        if ind in key:
+            if char in r_values[key]:
+                color = colors[i]
+                i+=1 
+                ax.plot(r_values[key][char], label=key, color=color)
+                ax.plot([j for j, p in enumerate(p_values[key][char]) if p and p<0.05],
+                    [r_values[key][char][j] for j, p in enumerate(p_values[key][char]) if p and p<0.05],
+                    marker='D', linestyle = 'None', color=color)
+
+    ax.set_xticks([i for i in range(0,12)])
+    ax.set_xticklabels(m_names)
+    ax.set_ylim([ylim0, ylim1])
+    ax.set_xlabel('Months')
+    ax.set_ylabel('Pearson R')
+    ax.set_title(f"{char_2_characteristic[char]} ({ind_titels[ind]})")
+    ax.legend(loc=(1.04,0))
+    return ax, fig
 
 
 def coh_corr_to_table(r_values, p_values, climate_data):
