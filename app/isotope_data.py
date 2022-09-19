ed908eaa3701c5f8fcdd7526f52edb747d1ea420
Mikhail <fff_khhh@mail.ru>
2022-09-19 21:10:18 +0700

    refactor(): rename class

---
+++ app/isotope_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from app.site_data import SiteData
+from typing import Dict
+
+
+class IsotopeData:
+    site: SiteData
+    isotope: str
+    data: pd.DataFrame
+
+    def __init__(self, data: pd.DataFrame, column: str, site: SiteData) -> None:
+        self.site = site
+        self.isotope = column.split('_')[1]
+        self.data = data[['Year', column]].rename(columns={column: 'Value'})
+    
+    def match(self, isotope: str, site_pattern: Dict) -> bool:
+        if self.isotope != isotope:
+            return False
+        
+        if not self.site.match(site_pattern):
+            return False
+        
+        return True
