c8516ac5d143a34e9cee32d828a298e62702a070
Mikhail <46995859+mikewellmeansme@users.noreply.github.com>
2022-12-05 16:58:18 +0700

    fix: add df length check

--- app/utils/comparison_functions.py
+++ app/utils/comparison_functions.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 from pandas import DataFrame
 from typing import Tuple
 from zhutils.correlation import dropna_pearsonr
 
 
 def compare_pearsonr(df: DataFrame, index: str) -> Tuple[float, float]:
+    if len(df.dropna()) < 2:
+        return np.nan, np.nan
     r, p = dropna_pearsonr(df[index], df['Value'])
     return r, p
